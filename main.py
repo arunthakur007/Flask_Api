ce1971c13652ce4865f1aac8c4d2dde40c61e690
arunthakur007 <arun0987vcodify@gmail.com>
2022-09-14 13:13:21 +0530

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-from number_check import number_checker
 from flask import Flask,render_template,request
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -9,19 +8,28 @@ app = Flask(__name__)
 url = "https://devapi.endato.com/PersonSearch"
 
 scope = ['https://www.googleapis.com/auth/spreadsheets',
-         'https://www.googleapis.com/auth/drive',
-         'https://www.googleapis.com/auth/drive.file']
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file']
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('crud.json', scope)
 client = gspread.authorize(credentials)
 sheet = client.open('test4')
 get_sheet = sheet.worksheet('Sheet1')
 
+def getRecord():
+    Google_sheet_value = get_sheet.get_all_values()
+    store_record = Google_sheet_value[1:]
+    return store_record
+
+def update_gsheet(payloads,sheet_range):
+    update_sheet = get_sheet.update(sheet_range,[payloads])
+    return update_sheet
 
 @app.route('/', methods=['GET', 'POST'])
 def Search():
-    all_datas = []
-    content = ''
+    sheet_range = 2
+    sheet_ranges = 1
+    count = 1
     context = {}
     all_data = list()
     try:
@@ -38,8 +46,14 @@ def Search():
             response = requests.post(url, json=payload, headers=headers)
             data = json.loads(response.text)
             get_data = data.get('persons')
-            emp = []
             for i in get_data:
+                # New.................................
+                dummy = i.get('associates')
+                for getdummy in dummy:
+                    fName = getdummy.get('name').get('firstName')
+                    LName = getdummy.get('name').get('lastName')
+                    SFullName  = fName+LName
+
                 firstName = i.get('name').get('firstName')
                 lastName = i.get('name').get('lastName')
                 fullName = firstName + ' ' + lastName
@@ -48,40 +62,35 @@ def Search():
                 addres = i.get('addresses')
                 for getAddress in addres:
                     addresses = getAddress.get('fullAddress')
-
-                    context = {
-                        "Name": fullName,
-                        "age": age,
-                        "Dob": dobFirstSeen,
-                        "addresses": addresses
-                    }
+                context = {
+                    "Name": fullName,
+                    "age": age,
+                    "Dob": dobFirstSeen,
+                    "addresses": addresses
+                }
                 all_data.append(context)
-
-                for key,value in context.items():
-                    all_datas.append((value))
-                sheet_range = 2
-                payload = [
-                    all_datas[0],
-                    all_datas[1],
-                    all_datas[2],
-                    all_datas[3]
-                ]
-                get_sheet.insert_row(payload, sheet_range)
+            for k in all_data:
+                SName = k.get('Name')
+                SAge = k.get('age')
+                SDob = k.get('Dob')
+                Saddress = k.get('addresses')
+                Spayload = [SName,SAge,SDob,Saddress]
+                GoogleSheetRecords = getRecord()
+                for getRcd in GoogleSheetRecords:
+                    if SName in getRcd:
+                        sheet_ranges = f'Sheet1!A{count}:D{count}'
+                        update_gsheet(Spayload,sheet_ranges)
+                    else:
+                        print(getRcd,"create","=====")
+                sheet_ranges +=1
+                get_sheet.insert_row(Spayload, sheet_range)
         return render_template('home.html',all_data=all_data)
     except:
         return render_template('home.html')
 
-@app.route('/api', methods=['GET', 'POST'])
-def api():
-    if request.method == 'GET':
-
-        phone = request.args['phone']
-        data = number_checker(phone)
-        print(data,"===")
-        # abc()
-        return data
-
 if __name__ == '__main__':
     app.run()
+
+
 
 
